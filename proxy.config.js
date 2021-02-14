37baa23d03429b78d098fc91f1ff2506872ffefe
Rusley Santos <rusley.santos@uniodigital.com.br>
2021-02-14 00:17:32 -0300

    altera proxy

--- proxy.config.js
+++
@@ -1,8 +0,0 @@
-const proxy = [{
-    context: ['/api'],
-    target: 'http://rusleysantos-001-site1.etempurl.com',
-    secure: false,
-    logLevel: 'debug',
-    //pathRewrite: {'^/api' : ''}
-}];
-module.exports = proxy;
